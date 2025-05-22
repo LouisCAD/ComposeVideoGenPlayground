649d691c9e2b5e3a69f10937dfa40bfef27cde89
Louis CAD <louis.cognault@gmail.com>
2025-05-22 22:31:35 +0200

    Add missing import

--- compose-to-video/src/desktopMain/kotlin/ComposeToVideo.kt
+++ compose-to-video/src/desktopMain/kotlin/ComposeToVideo.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.withIndex
+import kotlinx.coroutines.invoke
 import org.bytedeco.ffmpeg.ffmpeg
 import org.bytedeco.javacpp.Loader
 import org.jetbrains.skia.EncodedImageFormat

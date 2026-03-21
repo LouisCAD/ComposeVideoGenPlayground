f8daf36b3565897bfa273aad4d050ecd9975a9d9
Louis CAD <louis.cognault@gmail.com>
2026-03-21 10:31:19 +0100

    Create VideoGenProgressLine

--- composeApp/src/desktopMain/kotlin/com/louiscad/playground/compose/videogen/ui/components/VideoGenProgress.kt
+++ composeApp/src/desktopMain/kotlin/com/louiscad/playground/compose/videogen/ui/components/VideoGenProgress.kt
@@ -8,12 +8,63 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.louiscad.playground.compose.videogen.VideoGeneratorUi.FramesGenerationProgress
 import com.louiscad.playground.compose.videogen.core.FfmpegProgressLine
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+@Composable
+fun VideoGenProgressLine(
+    framesGenerationProgress: FramesGenerationProgress,
+    videoEncodingProgress: Flow<FfmpegProgressLine>,
+    modifier: Modifier = Modifier,
+) = Row(modifier, horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+    FramesGenerationProgressLine(framesGenerationProgress, Modifier.weight(1f))
+    VideoEncodingGenerationProgressLine(framesGenerationProgress, videoEncodingProgress, Modifier.weight(1f))
+}
+
+@Composable
+private fun FramesGenerationProgressLine(
+    framesGenerationProgress: FramesGenerationProgress,
+    modifier: Modifier = Modifier,
+) = Column(modifier) {
+    Row {
+        Text("Frames Generation")
+        Spacer(Modifier.weight(1f))
+        Text(text = with(framesGenerationProgress) { "$writtenFrames/$totalFrames" }, textAlign = TextAlign.End)
+    }
+    LinearProgressIndicator(
+        progress = framesGenerationProgress.let { it.writtenFrames.toFloat() / it.totalFrames },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+private fun VideoEncodingGenerationProgressLine(
+    framesGenerationProgress: FramesGenerationProgress,
+    videoEncodingProgress: Flow<FfmpegProgressLine>,
+    modifier: Modifier = Modifier,
+) = Column(modifier) {
+    val encodingProgressLine: FfmpegProgressLine? by videoEncodingProgress.collectAsState(initial = null)
+    val ratio = encodingProgressLine?.let {
+        it.frameNumber.toFloat() / framesGenerationProgress.totalFrames
+    } ?: 0f
+    Row {
+        Text("Video Encoding")
+        Spacer(Modifier.weight(1f))
+        val encodedFramesCount = encodingProgressLine?.frameNumber ?: 0
+        Text(text = with(framesGenerationProgress) { "$encodedFramesCount/$totalFrames" }, textAlign = TextAlign.End)
+    }
+    LinearProgressIndicator(
+        progress = ratio,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
 
 @Composable
 fun VideoGenProgressCards(
@@ -65,5 +116,42 @@ fun VideoGenProgressCards(
                 Text(text = encodingProgressLine?.let { "${it.fps}fps" } ?: "-", textAlign = TextAlign.End)
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun VideoGenProgressLinePreview() {
+    VideoGenProgressLine(
+        framesGenerationProgress = PreviewParams.framesGenerationProgress,
+        videoEncodingProgress = PreviewParams.videoEncodingProgress,
+    )
+}
+
+@Preview
+@Composable
+private fun VideoGenProgressCardsPreview() {
+    VideoGenProgressCards(
+        framesGenerationProgress = PreviewParams.framesGenerationProgress,
+        videoEncodingProgress = PreviewParams.videoEncodingProgress,
+    )
+}
+
+private object PreviewParams {
+    val framesGenerationProgress = FramesGenerationProgress.invoke(
+        totalFrames = 60 * 10,
+        writtenFramesState = mutableIntStateOf(60 * 10)
+    )
+    val videoEncodingProgress = flow {
+        val line = FfmpegProgressLine(
+            frameNumber = 65,
+            fps = 112.0,
+            quality = 1.0,
+            sizeKiB = 1024L,
+            time = FfmpegProgressLine.Time(hours = 0, minutes = 0, seconds = .5),
+            bitrateKpbs = 100.0,
+            speedFactor = 1.92f
+        )
+        emit(line)
     }
 }

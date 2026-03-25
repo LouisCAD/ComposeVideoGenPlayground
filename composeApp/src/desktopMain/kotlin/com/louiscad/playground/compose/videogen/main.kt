894797548b5666dac8cac7ed2425cc5b6c22e3a0
Louis CAD <louis.cognault@gmail.com>
2026-03-25 02:14:14 +0100

    Introduce more generic `VideoGenSetup` composable

--- composeApp/src/desktopMain/kotlin/com/louiscad/playground/compose/videogen/main.kt
+++ composeApp/src/desktopMain/kotlin/com/louiscad/playground/compose/videogen/main.kt
@@ -1,11 +1,16 @@
 package com.louiscad.playground.compose.videogen
 
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.window.ApplicationScope
 import androidx.compose.ui.window.MenuScope
 import androidx.compose.ui.window.Notification
@@ -14,7 +19,10 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberTrayState
 import androidx.compose.ui.window.rememberWindowState
+import com.louiscad.playground.compose.videogen.core.rememberIncrementCounter
+import com.louiscad.playground.compose.videogen.library.CounterOverlay
 import com.louiscad.playground.compose.videogen.ui.components.MediaGenJobList
+import com.louiscad.playground.compose.videogen.ui.components.VideoGenSetup
 import composevideogenplayground.composeapp.generated.resources.Res
 import composevideogenplayground.composeapp.generated.resources.video_template_24dp
 import kotlinx.coroutines.CoroutineScope
@@ -26,15 +34,33 @@ object AppDefaults {
     val iconsAutoMirrored = Icons.AutoMirrored.Rounded
 }
 
-private val mediaGenApp: MediaGenApp = MediaGenAppImpl(CoroutineScope(Dispatchers.Default))
 
 fun main() = application {
+    val defaultScope = rememberCoroutineScope({ Dispatchers.Default })
+    val mediaGenApp: MediaGenApp = remember { MediaGenAppImpl(defaultScope) }
     MediaGenTray(mediaGenApp)
 }
 
 @Composable
 private fun ApplicationScope.MediaGenTray(mediaGenApp: MediaGenApp) {
     val mainWindowState = rememberWindowState()
+    var showVideoGenSetup by remember { mutableStateOf(true) }
+
+    if (showVideoGenSetup) Window(onCloseRequest = { showVideoGenSetup = false }) {
+        VideoGenSetup(
+            contentToRecord = { sortedTriggerNanos ->
+                ProvideTextStyle(
+                    MaterialTheme.typography.h2.copy(
+                        color = Color.White,
+                        fontWeight = FontWeight.Medium
+                    )
+                ) {
+                    CounterOverlay(rememberIncrementCounter(sortedTriggerNanos))
+                }
+            },
+            onGenRequested = { mediaGenApp.addComposableToRecord(it) }
+        )
+    }
 
     var showJobs by remember { mutableStateOf(false) }
     if (showJobs) Window(onCloseRequest = { showJobs = false }) { MediaGenJobList(mediaGenApp) }
@@ -45,6 +71,7 @@ private fun ApplicationScope.MediaGenTray(mediaGenApp: MediaGenApp) {
             trayState.sendNotification(Notification(title = "Yolo?", message = "Hello World!", Notification.Type.Info))
         })
         Item("Show jobs window", enabled = !showJobs, onClick = { showJobs = true })
+        Item("Show video gen window", enabled = !showVideoGenSetup, onClick = { showVideoGenSetup = true })
         MediaGenAwareQuitItem(mediaGenApp, ::exitApplication)
     }
 }
---
+++ composeApp/src/desktopMain/kotlin/com/louiscad/playground/compose/videogen/ui/components/VideoGenSetup.kt
@@ -0,0 +1,172 @@
+package com.louiscad.playground.compose.videogen.ui.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.draganddrop.dragAndDropTarget
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draganddrop.DragData
+import androidx.compose.ui.draganddrop.dragData
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import com.louiscad.playground.compose.videogen.DragNDropTarget
+import com.louiscad.playground.compose.videogen.VideoGenerationRequest
+import com.louiscad.playground.compose.videogen.core.readTimecodes
+import com.louiscad.playground.compose.videogen.core.toNanosOffset
+import com.louiscad.playground.compose.videogen.ui.InputTransformations
+import com.louiscad.playground.compose.videogen.ui.OutputTransformations
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.invoke
+import kotlinx.coroutines.isActive
+import splitties.coroutines.call
+import splitties.coroutines.rememberCallableState
+import java.io.File
+import java.net.URI
+import java.nio.file.Paths
+import kotlin.time.Duration.Companion.nanoseconds
+import kotlin.time.Duration.Companion.seconds
+
+@Composable
+fun VideoGenSetup(
+    contentToRecord: @Composable (sortedTriggerNanos: LongArray) -> Unit,
+    onGenRequested: (VideoGenerationRequest) -> Unit
+) {
+
+    val startGeneratingRequest = rememberCallableState<Unit>()
+    val outputDirState = remember { mutableStateOf<File?>(null) }
+    val outputNameWithoutExtensionFieldState = remember { TextFieldState(initialText = "generated-video") }
+    val timeCodesSourceFileState = remember { mutableStateOf<File?>(null) }
+    var framesPerSecond: Int by remember { mutableIntStateOf(60) }
+    val secondsToRecordFieldState = remember { TextFieldState(initialText = "") }
+    val widthFieldState = remember { TextFieldState(initialText = "1920") }
+    val heightFieldState = remember { TextFieldState(initialText = "1080") }
+    val densityFieldState = remember { TextFieldState(initialText = "1.0") }
+
+    LaunchedEffect(Unit) {
+        while (isActive) {
+            val fps = framesPerSecond
+            startGeneratingRequest.awaitOneCall()
+            val output: File = outputDirState.value ?: continue
+            val _density = densityFieldState.text.toString().toFloatOrNull() ?: continue
+            val width = widthFieldState.text.toString().toInt()
+            val height = heightFieldState.text.toString().toInt()
+            val secondsToRecord = secondsToRecordFieldState.text.takeUnless { it.isEmpty() }?.toString()?.toDouble()
+            val timeCodesSourceFile = timeCodesSourceFileState.value ?: continue
+            val timeCodes = Dispatchers.IO {
+                readTimecodes(timeCodesSourceFile)
+            }.ifEmpty { null } ?: continue
+
+            val nanosOffsets = LongArray(timeCodes.size) { timeCodes[it].toNanosOffset(fps) }
+            println("WE HAVE ${nanosOffsets.size} timecodes")
+            val outputDuration = secondsToRecord?.seconds ?: nanosOffsets.last().nanoseconds
+            val density = Density(_density)
+            val outputFileNameWithoutExtension = outputNameWithoutExtensionFieldState.text.toString()
+            val request = VideoGenerationRequest(
+                outputDir = output,
+                outputFileNameWithoutExtension = outputFileNameWithoutExtension,
+                size = IntSize(width, height),
+                density = density,
+                duration = outputDuration,
+                framesPerSecond = fps,
+                getContent = { { contentToRecord(nanosOffsets) } }
+            )
+            onGenRequested(request)
+        }
+    }
+
+    Column(
+        modifier = Modifier.padding(16.dp).onPreviewKeyEvent { keyEvent ->
+            (keyEvent.key == Key.Enter).also { if (it) startGeneratingRequest.call() }
+        },
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        SizeLine(
+            widthFieldState = widthFieldState,
+            heightFieldState = heightFieldState,
+            densityFieldState = densityFieldState
+        )
+        Text("${MaterialTheme.typography.h2.fontSize}")
+        SecondsToRecordLine(secondsToRecordFieldState)
+        NameWithoutExtensionField(outputNameWithoutExtensionFieldState)
+        FileDragNDropTarget(state = outputDirState, label = "output dir", dir = true)
+        FileDragNDropTarget(state = timeCodesSourceFileState, label = "timecodes")
+        Text(text = "fps: $framesPerSecond (FYI)")
+        Button(
+            onClick = startGeneratingRequest,
+            enabled = startGeneratingRequest.isAwaitingCall
+        ) { Text("Generate") }
+    }
+}
+
+@Composable
+private fun NameWithoutExtensionField(outputNameWithoutExtensionFieldState: TextFieldState) = OutlinedTextField(
+    state = outputNameWithoutExtensionFieldState,
+    lineLimits = TextFieldLineLimits.SingleLine,
+    inputTransformation = InputTransformations.acceptableFilename,
+    outputTransformation = OutputTransformations.suffix(".mov"),
+    label = { Text("name") },
+)
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun FileDragNDropTarget(
+    state: MutableState<File?>,
+    label: String,
+    dir: Boolean = false
+) {
+    val dragAndDropTarget = remember {
+        DragNDropTarget { dropEvent ->
+            when (val data = dropEvent.dragData()) {
+                is DragData.FilesList -> {
+                    val fileUri = data.readFiles().singleOrNull() ?: return@DragNDropTarget false
+                    val targetFile = Paths.get(URI.create(fileUri)).toFile()
+                    if (dir != targetFile.isDirectory) return@DragNDropTarget false
+                    state.value = targetFile
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+    Row(
+        modifier = Modifier.padding(8.dp).dragAndDropTarget(
+            shouldStartDragAndDrop = { true },
+            target = dragAndDropTarget
+        ).let {
+            when {
+                dragAndDropTarget.canDrop -> it.border(width = 2.dp, color = Color.Green)
+                dragAndDropTarget.isDragging -> it.border(width = 2.dp, color = Color.LightGray)
+                else -> it
+            }
+        }
+    ) {
+        val file by state
+        Column {
+            Text(label)
+            Text(text = file?.name ?: "?")
+        }
+        Spacer(Modifier.weight(1f))
+//            Box(
+//                Modifier.size(48.dp).background(
+//                    color = when {
+//                        dragAndDropTarget.canDrop -> Color.Green
+//                        dragAndDropTarget.isDragging -> Color.Cyan
+//                        else -> Color.LightGray
+//                    }
+//                )
+//            )
+    }
+}

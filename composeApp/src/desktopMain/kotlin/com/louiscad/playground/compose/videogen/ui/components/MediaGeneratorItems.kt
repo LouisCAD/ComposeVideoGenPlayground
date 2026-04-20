0e915ab43bb1fe045d877c35ed571a91728ba19a
Louis CAD <louis.cognault@gmail.com>
2026-04-21 00:21:13 +0200

    Tweak alignment of Mercedes vs Bacchetta overlay

--- composeApp/src/desktopMain/kotlin/com/louiscad/playground/compose/videogen/ui/components/MediaGeneratorItems.kt
+++ composeApp/src/desktopMain/kotlin/com/louiscad/playground/compose/videogen/ui/components/MediaGeneratorItems.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.IntState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -58,8 +59,10 @@ val mediaGeneratorItems: List<MediaGeneratorItem> = listOf(
         defaultDensity = 1f, //TODO: Do we need an explicit, or inferred preview density?
         previewScale = 1 / 8f,
         content = MediaGeneratorItem.Content.CounterBasedVideo { counter ->
-            ProvideRelativeDensity(Density(7f)) {
-                MercedesVsBacchetta(counter?.toStepState() ?: rememberAutoAdvancingStepsState())
+            Box(Modifier.fillMaxSize(), contentAlignment = BiasAlignment(horizontalBias = 0f, verticalBias = -.8f)) {
+                ProvideRelativeDensity(Density(7f)) {
+                    MercedesVsBacchetta(counter?.toStepState() ?: rememberAutoAdvancingStepsState())
+                }
             }
         }
     ),
